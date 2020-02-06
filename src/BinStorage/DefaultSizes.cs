65a04b0baa51e5653558e2b1fc633f1a7f511560
Musa Alp <musaa.alp@gmail.com>
2020-02-06 12:03:42 +0300

    feat: Add constants related memory sizes

---
+++ src/BinStorage/DefaultSizes.cs
@@ -0,0 +1,17 @@
+﻿namespace BinStorage
+{
+    public class DefaultSizes
+    {
+        public const int CursorHolderOffset = 0;
+        public const int CursorHolderSize = sizeof(long);
+        public const int Md5HashSize = 16;
+        public const int IndexDataSize = Md5HashSize + sizeof(long) + sizeof(long);
+        public const int KeyOffsetSize = sizeof(long);
+        public const int KeySizeSize = sizeof(int);
+        public const int DefaultNodeSize = KeyOffsetSize + KeySizeSize + IndexDataSize;
+
+        public const int DefaultReadBufferSize = Sizes.Size16Kb;
+        public const long DefaultStorageBinCapacity = Sizes.Size1Gb;
+        public const long DefaultIndexBinCapacity = Sizes.Size16Mb;
+    }
+}
---
+++ src/BinStorage/Sizes.cs
@@ -0,0 +1,16 @@
+﻿namespace BinStorage
+{
+    public static class Sizes
+    {
+        public const int Size1Kb = 1024;
+        public const int Size4Kb = Size1Kb * 4;
+        public const int Size10Kb = Size1Kb * 10;
+        public const int Size16Kb = Size1Kb * 16;
+        public const int Size1Mb = Size1Kb * Size1Kb;
+        public const int Size4Mb = Size1Mb * 4;
+        public const int Size16Mb = Size1Mb * 16;
+        public const long Size1Gb = Size1Mb * Size1Kb;
+        public const long Size5Gb = Size1Gb * 5;
+        public const long Size1Tb = Size1Gb * Size1Kb;
+    }
+}
